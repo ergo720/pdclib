78305d1a215a2211db5312e81bf5efb3e7450874
ergo720 <45463469+ergo720@users.noreply.github.com>
2023-09-10 23:15:37 +0200

    Added custom xbox assertion implementation

---
+++ platform/xbox/functions/_PDCLIB/assert.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Defined in cxbxrkrnl
+extern void __stdcall RtlAssert(void *FailedAssertion, void *FileName, uint32_t LineNumber, char *Message);
+
+void _xbox_assert(char const *const expression, char const *const file_name, char const *const function_name, unsigned long line)
+{
+	char buffer[512];
+	snprintf(buffer, 512, "In function '%s': ", function_name);
+	RtlAssert((void *)expression, (void *)file_name, line, buffer);
+}
